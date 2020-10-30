855d6ce6fb533842a87d1326e0f1ad82d8e679f1
Aditi Gupta <60029443+Aditi200@users.noreply.github.com>
2020-10-31 02:37:13 +0530

    Add files via upload

---
+++ Number of Longest Increasing Subsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+    int len = 0; // len of sequence
+ 
+    for(int num: nums){
+        int idx = Arrays.binarySearch(dp, 0, len, num);
+ 
+        //if not found, return binarySearch return -insertPosition-1
+        if(idx < 0){
+            idx = - (idx + 1);
+        }
+ 
+        dp[idx] = num;
+ 
+        //update len when insert position is at the end
+        if(idx==len){
+            len++;
+        }
+    }
+ 
+    return len;
+    }
+}
